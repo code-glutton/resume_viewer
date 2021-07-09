f3779bbc402b7a66556773d812a6fbab55af5222
Osahon Jonathan-Odia <emmyodia@gmail.com>
2021-07-09 05:11:40 +0100

    json data fetched

--- src/App.js
+++ src/App.js
@@ -4,18 +4,52 @@ import Contact from './components/contact';
 import Footer from './components/footer';
 import Portfolio from './components/portfolio';
 import Resume from './components/resume';
+import { Component } from 'react';
+import $ from 'jquery';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <About/>
-      <Resume/>
-      <Portfolio/>
-      <Contact/>
-      <Footer/>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      foo:"bar",
+      resumeData:{}
+    }
+  }
+
+getResumeData = () => {
+  $.ajax({
+    url:'http://localhost:3000/js/resumeData.json',
+    dataType:'json',
+    cache:false,
+    success: function(data){
+      this.setState({
+        resumeData:data
+      })
+    }.bind(this),
+    error: function(xhr, status, error){
+      console.log(error);
+      alert(error);
+    }.bind(this)
+  })
+}
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render(){
+    console.log(this.state.resumeData);
+    return (
+      <div className="App">
+        <Header/>
+        <About/>
+        <Resume/>
+        <Portfolio/>
+        <Contact/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
